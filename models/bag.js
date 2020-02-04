670f4982fbf8f7c1c507cefea3fdbd4b64069452
danieljbuyinza <danieljbuyinza@gmail.com>
2020-02-04 10:19:37 +0300

    Created common, models and test folders, index.js file, and 3 classes in models - laptop, bag, speakers

---
+++ models/bag.js
@@ -0,0 +1,7 @@
+class Bag {
+    constructor(colour, numberOfPockets, price) {
+        this.colour = colour;
+        this.numberOfPockets = numberOfPockets;
+        this.price = price;
+    }
+}
---
+++ models/speakers.js
@@ -0,0 +1,6 @@
+class Speakers {
+    constructor(numberOfSpeakers, bluetooth) {
+        this.numberOfSpeakers = numberOfSpeakers;
+        this.bluetooth = bluetooth;
+    }
+}
